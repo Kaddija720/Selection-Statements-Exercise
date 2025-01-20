4d9a61724e55b2d5abc3a54fbd57080d2a6d7298
Kaddijatou Hydara <kad67414@gmail.com>
2025-01-20 09:53:01 -0500

    added game lop

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var favNumber = random.Next(1,11);
+            int response = 0;
+
+            while (response != favNumber)
+            {
+                Console.WriteLine("Give me a number between 1 and 10");
+                int reponse = int.Parse(Console.ReadLine());
+
+                if (reponse < favNumber)
+                {
+                    Console.WriteLine($"Too low!\nYour guess: {reponse}");
+                }
+                else if (reponse > favNumber)
+                {
+                    Console.WriteLine($"Too high!\nYour guess: {reponse}");  
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!\nThe favorite number was {favNumber}!");
+                }   
+            }
+            
+            
         }
     }
 }
